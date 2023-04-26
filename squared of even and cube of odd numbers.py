d4f875de645f9513d0167c8c784cf0eeca4f38a7
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-26 22:27:13 +0800

    pseudocode

---
+++ squared of even and cube of odd numbers.py
@@ -0,0 +1,25 @@
+# Ray Allessandra D. Pacis | BSCPE 1-5
+# Write a method in python that will create two separate text files after reading the source text file named integers.txt that contains 20 integers. 
+# The first output file will be named double.txt containing the square of all even integers found in integers.txt.
+# The second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt.
+
+# open files integers.txt, double.txt, and triple.txt
+
+# ask user for numbers
+# check if inputs are integers
+
+# parse the inputs
+
+# put the numbers to integers.txt
+
+# read integers.txt by line
+
+# if extracted number is even
+    # extracted number will be squared
+    # write squared number to double.txt
+
+# else if the extracted number is odd
+    # extracted number will be cubed
+    # write cubed number to triple.txt
+
+# end of program
