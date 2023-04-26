f9f5099bca8c1633ceaf5fc34cb22ce08d063669
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-27 00:00:46 +0800

    oh no

--- squared of even and cube of odd numbers.py
+++ squared of even and cube of odd numbers.py
@@ -14,17 +14,16 @@ with open("integers.txt", 'w') as input_file, open("double.txt", 'w') as even_sq
         # put the numbers to integers.txt
         input_file.write((user_numbers) + '\n')
         i += 1
-        break
 
     # read integers.txt by line
-    for line input_file:
+    for line in input_file:
         user_num = int(line)
             
-                # if extracted number is even
+        # if extracted number is even
         if user_num % 2 == 0:
-                # extracted number will be squared
+            # extracted number will be squared
             squared_number = user_num ** 2
-                # write squared number to double.txt
+            # write squared number to double.txt
             even_squared.write(str(squared_number) + '\n')
 
 # else if the extracted number is odd
