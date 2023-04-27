be48158e8825fa66dad2fe13c3d5eee8636f0b99
rayrayyrayyy <allssndr.ray@gmail.com>
2023-04-27 11:18:53 +0800

    boom intro

--- squared of even and cube of odd numbers.py
+++ squared of even and cube of odd numbers.py
@@ -12,14 +12,25 @@ from colorama import Fore, Back, Style
 print_grey = lambda x: cprint(x, 'grey')
 print_yellow = lambda x: cprint(x, 'yellow')
 
+# intro
+hi_welcome = pyfiglet.figlet_format("Welcome User!", font = "doom", width = 150, justify = "center")
+print(Fore.YELLOW + hi_welcome)
+print(Style.RESET_ALL)
+print(Fore.BLUE + "="*150)
+instruction = "INSTRUCTION: Please enter 20 random integers and if it's even I will find its squared and if it's odd I'll cube it.☺"
+instruction_center = instruction.center(150)
+print(instruction_center)
+print(Fore.BLUE + "="*150)
+print(Style.RESET_ALL)
+
 # open file integers.txt(write)
 with open("integers.txt", 'w') as input_file:
 
     # create loop
     i = 0
-    while (i < 2):
+    while (i < 20):
         # ask user for numbers
-        user_numbers = input("Please write a number: ")
+        user_numbers = input("\t\tPlease write a number: ")
         # put the numbers to integers.txt
         input_file.write((user_numbers) + '\n')
         i += 1
@@ -45,9 +56,10 @@ with open("integers.txt", 'r') as integers_file, open("double.txt", 'w') as even
             odd_cube.write(str(cubed_number) + '\n')
 
 # outro
-done = pyfiglet.figlet_format("Thank you!", font = 'doom', width = 100, justify = 'center')
+print('\n' + Fore.YELLOW + Style.BRIGHT + "-"*150)
+done = pyfiglet.figlet_format("Thank you!", font = 'doom', width = 150, justify = 'center')
 print_yellow(done)
 note = "See text files for results"
-print_grey(Fore.YELLOW + Style.BRIGHT + '-'*38 + note + '-'*38 + '\n') 
+print_grey(Fore.YELLOW + '-'*62 + note + '-'*62 + '\n') 
 
 # end of program
